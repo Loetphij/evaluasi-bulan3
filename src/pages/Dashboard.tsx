44bfe79ff2f880bd3abe7e4450ef1a368b92e20f
loetfij <luoffgemink769@gmail.com>
2025-10-28 14:17:58 +0700

    Evaluasi Bulan ke-3 ygy

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import { useState, useCallback } from "react";
 import { useProducts } from "../hooks/useProducts";
 import { ProductForm } from "../components/ProductForm";
 import type { Product } from "../contexts/ProductContext";
